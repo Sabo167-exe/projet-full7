25cdd746e86e8f555ad5818c825cea7076ed15fb
Sabo167-exe <adrienfouchet1712@gmail.com>
2026-09-25 11:45:26 +0200

    bug theme fix

--- page/js/toogleconsole.js
+++ page/js/toogleconsole.js
@@ -13,21 +13,38 @@ if (themeBtn) {
         const isLight = document.documentElement.classList.toggle('light');
         if (isLight) {
             themeBtn.textContent = '☀️';
-          } else {
+        } else {
             themeBtn.textContent = '🌙';
-          }
+        }
         
     });
 }
 document.addEventListener("keydown", (event) => {
-
+    
     if (event.key === "n" || event.key === "N"){
         const isLight = document.documentElement.classList.toggle('light');
         if (isLight) {
             themeBtn.textContent = '☀️';
-          } else {
+        } else {
             themeBtn.textContent = '🌙';
-          }
+        }
         
     }
 })
+
+function toggleConsole(brand) {
+    const show = document.querySelector(`.${brand} .page-collection-marque-show`);
+    const consoles = document.querySelectorAll(`.${brand} .page-collection-marque-consoles`);
+    
+    show.addEventListener("click", () => {
+        show.classList.toggle("active");
+        
+        consoles.forEach((console) => {
+            console.classList.toggle("maxify");
+        });
+    });
+}
+
+toggleConsole("nintendo");
+toggleConsole("playstation");
+toggleConsole("xbox");
